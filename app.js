fafb010a8cb036ea6e5ea250491eaf1000511e59
LogicIsPlantingBomb <logicunit64@gmail.com>
2025-02-16 03:11:18 +0530

    Done

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ const express = require("express");
 const cors = require('cors');
 const app = express();
 const connectToDb = require('./db/db');
-const userRoutes = require('./routes/user.routes')
-const cookieparser = require('cookie-parser')
+const userRoutes = require('./routes/user.routes');
+const cookieparser = require('cookie-parser');
 
 connectToDb();
 
-// Remove the default cors middleware
-// app.use(cors()); <- Remove this line
-
 // Use the configured cors middleware
 app.use(cors({
-  origin: 'process.env.FRONTEND_URL',
+  origin: process.env.FRONTEND_URL, // Use the value of the environment variable
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
+// Handle preflight requests
+app.options('*', cors());
+
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send("backend is running");
 });
 
-// Fix the API route to match your frontend fetch URL
-app.use('/api', userRoutes);  // Changed from '/users' to '/api'
+// Mount user routes under /api
+app.use('/api', userRoutes);
 
 module.exports = app;
